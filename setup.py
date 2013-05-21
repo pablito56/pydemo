8f56d4e58ee277f62d4e316e9fe7547ea0f46fd9
pablito56 <pablito56@gmail.com>
2013-05-21 08:28:35 +0200

    Enhanced setup.py to retry in case readline installation failed (to be improved)

--- setup.py
+++ setup.py
@@ -7,34 +7,46 @@ import platform
 import os
 
 
-def get_requirements():
+def _replace_readline(req):
+    if req.startswith("readline"):
+        return "pyreadline"
+    return req
+
+
+def get_requirements(replace_readline=False):
     reqs_file = 'requirements_win.txt' if platform.system().lower() == "windows" else 'requirements.txt'
     try:
         with open(reqs_file) as reqs_file:
             reqs = filter(None, imap(lambda line: line.replace('\n', '').strip(), reqs_file))
+            if replace_readline:
+                reqs = map(_replace_readline, reqs)
             return reqs
     except IOError:
         pass
     return []
 
-
-setup(
-    name="pydemo",
-    version="0.0.4",
-    description="Python code demonstration console for didactic purposes",
-    long_description="Python code demonstration console for didactic purposes. \
-Prints and executes input files in blocks of lines. Extends code.InteractiveConsole",
-    author="Pablo Enfedaque",
-    author_email='pablito56@gmail.com',
-    url="https://github.com/pablito56/pydemo",
-    packages=find_packages(exclude=['test*']),
-    include_package_data=True,
-    entry_points={
-        'console_scripts': [
-            'pydemo = pydemo.pydemo:main',
-        ]
-    },
-    install_requires=get_requirements(),
-    test_suite='nose.collector',
-    tests_require="nose",
-)
+for reqs in (get_requirements(), get_requirements(True)):
+    try:
+        setup(
+            name="pydemo",
+            version="0.0.5",
+            description="Python code demonstration console for didactic purposes",
+            long_description="Python code demonstration console for didactic purposes. \
+        Prints and executes input files in blocks of lines. Extends code.InteractiveConsole",
+            author="Pablo Enfedaque",
+            author_email='pablito56@gmail.com',
+            url="https://github.com/pablito56/pydemo",
+            packages=find_packages(exclude=['test*']),
+            include_package_data=True,
+            entry_points={
+                'console_scripts': [
+                    'pydemo = pydemo.pydemo:main',
+                ]
+            },
+            install_requires=reqs,
+            test_suite='nose.collector',
+            tests_require="nose",
+        )
+        break
+    except SystemExit:
+        print "\n\tCaught SystemExit. Trying alternative requirements\n"
